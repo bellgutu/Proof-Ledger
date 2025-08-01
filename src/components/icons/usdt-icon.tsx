6e8baaa05d4a62fc3cdcd04f239cbe4ff1061a01
Abel Gutu <abelgutu@gmail.com>
2025-08-01 21:15:45 +0000

    The symols for the token we Jus add it is not right one use the same log

--- src/components/icons/usdt-icon.tsx
+++ src/components/icons/usdt-icon.tsx
@@ -2,6 +2,6 @@ import * as React from "react";
 
 export const UsdtIcon = (props: React.SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" {...props}>
-    <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 14.5h-5v-3h1.5v1.5h2v-5H9v1.5H7.5v-3h7v3H13v5h2v-1.5h1.5v3z"/>
+    <path d="M12 2c5.523 0 10 4.477 10 10s-4.477 10-10 10S2 17.523 2 12 6.477 2 12 2zm4.49 6.51h-3.98v-2h8v2h-4.02v8h-2.5v-8zm-9.51 0h-2.5v8h2.5v-3.49h3.49v-2H6.98V8.51z"/>
   </svg>
 );

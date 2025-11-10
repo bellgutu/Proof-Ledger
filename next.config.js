5fa802c1816dad8f605f024612b9fd2186b9b9b9
Abel Gutu <abelgutu@gmail.com>
2025-11-10 15:18:02 +0000

    I want you to update all the documention for this platform and the capab

--- next.config.js
+++ next.config.js
@@ -10,24 +10,7 @@ const nextConfig = {
   },
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'assets.coingecko.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'api.coingecko.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'pepecoin.org',
-        port: '',
-        pathname: '/**',
-      }
+
     ],
   },
 };

88ab6c05246fcbe92a30b317374c4c6eccf32f12
Abel Gutu <abelgutu@gmail.com>
2025-11-13 12:23:10 +0000

    I just uninstalled metamask Just to make a point and now there is nothin

--- src/contexts/wagmi-config.ts
+++ src/contexts/wagmi-config.ts
@@ -5,16 +5,18 @@ import { createConfig, http } from 'wagmi';
 import { mainnet, sepolia } from 'wagmi/chains';
 import { createWeb3Modal } from '@web3modal/wagmi/react';
 
+// 1. Get projectId from https://cloud.walletconnect.com
 const projectId = process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID;
 
 if (!projectId) {
   throw new Error('NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID is not set')
 }
 
+// 2. Create wagmiConfig
 const metadata = {
   name: 'Enterprise Verification Platform',
   description: 'A closed-loop system for end-to-end verification of shipping, insurance, and quality control.',
-  url: 'https://web3modal.com',
+  url: 'https://web3modal.com', // origin must match your domain & subdomain
   icons: ['https://avatars.githubusercontent.com/u/37784886']
 }
 
@@ -26,12 +28,14 @@ export const wagmiConfig = createConfig({
     [mainnet.id]: http(),
     [sepolia.id]: http(),
   },
-  ssr: false, // Required for Web3Modal
+  // In a Next.js app, you need to set `ssr: false` to avoid hydration errors.
+  ssr: false, 
 });
 
+// 3. Create modal
 createWeb3Modal({
   wagmiConfig,
   projectId,
   metadata,
-  enableAnalytics: false
+  enableAnalytics: false // Optional - defaults to your Cloud configuration
 })

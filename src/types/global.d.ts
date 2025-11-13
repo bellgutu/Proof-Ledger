9e58a192a710afe8f48de3c45267846bfaf7fff9
Abel Gutu <abelgutu@gmail.com>
2025-11-13 12:37:42 +0000

    Now the wallet connect button is gone i think that is better than a non

---
+++ src/types/global.d.ts
@@ -0,0 +1,7 @@
+
+declare namespace JSX {
+  interface IntrinsicElements {
+    'w3m-connect-button': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+    'w3m-network-button': React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLElement>;
+  }
+}

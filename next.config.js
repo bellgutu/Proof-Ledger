409d2d0f06acd87076defddb3e4fee0e826ad0ae
Abel Gutu <abelgutu@gmail.com>
2025-11-14 22:22:19 +0000

    2025-11-14T22:19:45Z [web] > proof-ledger@0.1.0 dev 2025-11-14T22:19:45Z

--- next.config.js
+++ next.config.js
@@ -20,12 +20,6 @@ const nextConfig = {
         }
     ],
   },
-  webpack: (config) => {
-    config.externals.push("pino-pretty", "lokijs", "encoding");
-    return config;
-  },
 };
 
 module.exports = nextConfig;
-
-    

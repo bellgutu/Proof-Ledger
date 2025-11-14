33e4dfcfc9bef8d0ea575bc756a7f5dc6978dd75
Abel Gutu <abelgutu@gmail.com>
2025-11-14 23:09:07 +0000

    025-11-14T23:07:36Z [web] Module not found: Can't resolve 'encoding' in

---
+++ proof-ledger/next.config.js
@@ -0,0 +1,29 @@
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+        {
+            protocol: 'https',
+            hostname: 'avatars.githubusercontent.com',
+        },
+        {
+            protocol: 'https',
+            hostname: 'picsum.photos',
+        }
+    ],
+  },
+  webpack: (config) => {
+    config.externals.push("pino-pretty", "lokijs", "encoding");
+    return config;
+  },
+};
+
+module.exports = nextConfig;

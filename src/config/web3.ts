7e6f5bba210ac8b220f46dbee2674a7522e189b4
Abel Gutu <abelgutu@gmail.com>
2025-11-24 12:36:21 +0000

    still getting the same error

--- src/config/web3.ts
+++ src/config/web3.ts
@@ -7,17 +7,10 @@ export const projectId = process.env.NEXT_PUBLIC_WC_PROJECT_ID;
 
 if (!projectId) throw new Error('NEXT_PUBLIC_WC_PROJECT_ID is not set');
 
-const getUrl = () => {
-  if (typeof window === 'undefined') {
-    return 'https://proof-ledger.app';
-  }
-  return window.location.origin;
-}
-
 export const metadata = {
   name: 'Proof Ledger',
   description: 'A closed-loop system for end-to-end verification of shipping, insurance, and quality control.',
-  url: getUrl(),
+  url: 'https://proof-ledger.app',
   icons: ['https://avatars.githubusercontent.com/u/37784886']
 };
 
@@ -32,5 +25,5 @@ export const config = createConfig({
     walletConnect({ projectId, metadata, showQrModal: false }),
     injected({ shimDisconnect: true }),
   ],
-  ssr: false, 
+  ssr: false, // Explicitly disable SSR to ensure client-only operation
 });

4b5b2f55a30b35d1bdee7257e67e7b32f64a3d2d
Abel Gutu <abelgutu@gmail.com>
2025-11-24 00:53:06 +0000

    Unhandled Runtime Error Error: No matching key. session topic doesn't ex

--- src/config/web3.ts
+++ src/config/web3.ts
@@ -1,4 +1,5 @@
 'use client';
+import { createWeb3Modal } from '@web3modal/ethers/react';
 import { http, createConfig } from 'wagmi';
 import { walletConnect, injected } from 'wagmi/connectors';
 import { sepolia } from 'wagmi/chains';
@@ -27,4 +28,19 @@ export const config = createConfig({
     injected({ shimDisconnect: true }),
   ],
   ssr: true, // Enable SSR
+});
+
+// 4. Create a Web3Modal instance
+createWeb3Modal({
+  ethersConfig: config,
+  chains: [sepolia],
+  projectId,
+  enableAnalytics: true,
+  themeVariables: {
+    '--w3m-accent': 'hsl(250 80% 60%)',
+    '--w3m-color-mix': 'hsl(220 10% 18%)',
+    '--w3m-color-mix-strength': 20,
+    '--w3m-border-radius-master': '1px',
+    '--w3m-font-family': 'Inter, sans-serif',
+  }
 });

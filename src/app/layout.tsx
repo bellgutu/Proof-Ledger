e58c236c088d5145ec6ae89c71bb7db98b299b0a
Abel Gutu <abelgutu@gmail.com>
2025-11-24 00:47:03 +0000

    2025-11-24T00:44:17Z [web] <Firebase Studio> Starting preview... 2025-11

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,7 @@ import AppShell from '@/components/app-shell';
 import Web3Provider from '@/components/web3-provider';
 import { headers } from 'next/headers';
 import { cookieToInitialState } from 'wagmi';
-import { config } from '@/config/web3';
+import { config } from '@/config/web3.server';
 
 export const metadata: Metadata = {
   title: 'Proof Ledger',

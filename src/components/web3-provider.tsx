60f999cae08e45fbf9b5b05afc764a2eaff5bfe5
Abel Gutu <abelgutu@gmail.com>
2025-11-24 11:41:20 +0000

    Unhandled Runtime Error ChunkLoadError: Loading chunk app/layout failed.

--- src/components/web3-provider.tsx
+++ src/components/web3-provider.tsx
@@ -1,22 +1,37 @@
 'use client';
 
 import React from 'react';
-import { config } from '@/config/web3';
-import { WagmiProvider, type State } from 'wagmi'
+import { config, projectId } from '@/config/web3';
+import { createWeb3Modal } from '@web3modal/ethers/react'
+import { WagmiProvider } from 'wagmi'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
 // Setup queryClient
 const queryClient = new QueryClient()
 
+// Create a Web3Modal instance
+createWeb3Modal({
+  ethersConfig: config,
+  chains: [config.chains[0]],
+  projectId: projectId!,
+  enableAnalytics: true,
+  themeVariables: {
+    '--w3m-accent': 'hsl(250 80% 60%)',
+    '--w3m-color-mix': 'hsl(220 10% 18%)',
+    '--w3m-color-mix-strength': 20,
+    '--w3m-border-radius-master': '1px',
+    '--w3m-font-family': 'Inter, sans-serif',
+  }
+});
+
+
 export default function Web3Provider({
-  children,
-  initialState
+  children
 }: {
   children: React.ReactNode;
-  initialState?: State;
 }) {
   return (
-    <WagmiProvider config={config} initialState={initialState}>
+    <WagmiProvider config={config}>
       <QueryClientProvider client={queryClient}>
         {children}
       </QueryClientProvider>
--- src/config/web3.ts
+++ src/config/web3.ts
@@ -1,6 +1,5 @@
 'use client';
-import { createWeb3Modal } from '@web3modal/ethers/react';
-import { http, createConfig } from 'wagmi';
+import { defaultWagmiConfig } from '@web3modal/ethers/react';
 import { walletConnect, injected } from 'wagmi/connectors';
 import { sepolia } from 'wagmi/chains';
 
@@ -18,29 +17,13 @@ const metadata = {
 };
 
 // 3. Create a unified config
-export const config = createConfig({
+export const config = defaultWagmiConfig({
   chains: [sepolia],
-  transports: {
-    [sepolia.id]: http(),
-  },
+  projectId,
+  metadata,
   connectors: [
     walletConnect({ projectId, metadata, showQrModal: false }),
     injected({ shimDisconnect: true }),
   ],
-  ssr: false, // Ensure client-side only state management
-});
-
-// 4. Create a Web3Modal instance. This is called only once.
-createWeb3Modal({
-  ethersConfig: config,
-  chains: [sepolia],
-  projectId,
-  enableAnalytics: true,
-  themeVariables: {
-    '--w3m-accent': 'hsl(250 80% 60%)',
-    '--w3m-color-mix': 'hsl(220 10% 18%)',
-    '--w3m-color-mix-strength': 20,
-    '--w3m-border-radius-master': '1px',
-    '--w3m-font-family': 'Inter, sans-serif',
-  }
+  ssr: false, // Client-side only
 });
